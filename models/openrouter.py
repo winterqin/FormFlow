b766072b2e17cd34e085d2d8b3997a58dc3223ab
QinWinter <qinwinter@qq.com>
2025-09-14 17:03:27 +0800

    fill form

--- models/openrouter.py
+++ models/openrouter.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 load_dotenv()
+
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
   api_key=os.getenv("OPENROUTER_API_KEY"),
